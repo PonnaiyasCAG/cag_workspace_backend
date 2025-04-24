c2223ecbf5a0214226f27869e20b9cdf7d68cc45
PonnaiyasCAG <ponnaiyacag@gmail.com>
2025-04-24 23:20:58 +0530

    cross Origin added

--- src/main/java/net/workspace/workspace_backend/controller/UserDetailsController.java
+++ src/main/java/net/workspace/workspace_backend/controller/UserDetailsController.java
@@ -6,14 +6,12 @@ import net.workspace.workspace_backend.model.UserDetails;
 import net.workspace.workspace_backend.model.UserResponse;
 import net.workspace.workspace_backend.service.UserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class UserDetailsController {
 
     @Autowired
--- src/main/java/net/workspace/workspace_backend/controller/fileController.java
+++ src/main/java/net/workspace/workspace_backend/controller/fileController.java
@@ -9,6 +9,8 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/files")
+@CrossOrigin
+
 public class fileController {
 
   /*  @Autowired
